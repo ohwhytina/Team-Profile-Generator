32ccb7760991efcc5ca6458f84d43231e835f106
Tina Nguyen <nguyentinaca@yahoo.com>
2021-03-03 12:51:59 -0800

    redo repository

---
+++ index.js
@@ -0,0 +1,137 @@
+const inquirer = require('inquirer');
+// const generatePage = require('./src/page-template');
+
+// const Employee = require('./lib/Employee.js');
+// const Manager = require('./lib/Manager.js');
+// const Engineer = require('./lib/Engineer.js');
+// const Intern = require('./lib/Intern.js');
+var teamArray = [];
+
+
+const promptQuestions = () => {
+    return inquirer.prompt([
+        {
+            type: 'list',
+            name: 'role',
+            message: "Add a team member:",
+            choices: ['Manager', 'Engineer', 'Intern', 'No, thank you']
+        }
+    ])
+    .then (function(member) {
+        if (member.role === 'Manager') {
+          inquirer.prompt ([
+            {
+                type: 'input',
+                name: 'name',
+                message: "Enter team manager's name:",
+            },
+            {
+                type: 'input',
+                name: 'id',
+                message: "Enter team manager's employee id (if applicable)",
+            },
+            {
+                type: 'input',
+                name: 'email',
+                message: "Enter team manager's email:",
+            },   
+            {
+                type: 'number',
+                name: 'phone',
+                message: "Enter team manager's office number:",
+            }
+          ])
+          .then (function(mgrRole) {
+              var newMgr = new Manager(mgrRole.name, mgrRole.id, mgrRole.email, mgrRole.phone)
+              teamArray.push(newMgr);
+              console.log(mgrRole)
+              addMember();
+          });
+          
+        } else if (member.role === 'Engineer') {
+            inquirer.prompt ([
+                {
+                    type: 'input',
+                    name: 'name',
+                    message: "What is your Engineer's name?",
+                },
+                {
+                    type: 'input',
+                    name: 'id',
+                    message: "What is your Engineer's ID?",
+                },
+                {
+                    type: 'input',
+                    name: 'email',
+                    message: "What is your Engineer's email?",
+                },
+                {
+                    type: 'input',
+                    name: 'git',
+                    message: "What is your Engineer's github username?",
+                }
+            ])
+                .then (function(engRole) {
+                    var newEng = new Engineer(engRole.name, engRole.id, engRole.email, engRole.git)
+                    teamArray.push(newEng);
+                    console.log(engRole);
+                    addMember();
+                });
+
+        } else if (member.role === 'Intern') {
+                inquirer.prompt ([
+                    {
+                        type: 'input',
+                        name: 'name',
+                        message: "What is your Intern's name?",
+                    },
+                    {
+                        type: 'input',
+                        name: 'id',
+                        message: "What is your Intern's ID?",
+                    },
+                    {
+                        type: 'input',
+                        name: 'email',
+                        message: "What is your Intern's email?",
+                    },
+                    {
+                        type: 'input',
+                        name: 'school',
+                        message: "What is your Intern's school name?",
+                    },
+                ])
+                .then (function(intRole) {
+                    var newInt = new Intern(intRole.name, intRole.id, intRole.email, intRole.school)
+                    teamArray.push(newInt);
+                    console.log(intRole);
+                    addMember();
+                });
+        }
+       
+    });
+}; 
+
+function addMember () {
+    inquirer.prompt([
+    {   
+        name: "additionalMem",
+        message: "Do you want to add another team member?",
+        type: "confirm",
+        default: false
+    }   
+    ])
+    .then(function(addMore){
+        if(addMore.additionalMem) {
+            return promptQuestions();
+        }  
+    });
+}
+
+promptQuestions()
+    .then(memberData => {
+        return generatePage(teamArray);
+    })
+    .then(pageHTML => {
+        return writeFile(pageHTML)
+    });
---
+++ src/page-template.js
@@ -0,0 +1,71 @@
+const displayInfo = team => {
+  if (team.role === "Manager") {
+      console.log(employee.phone);
+      return `office number: ${employee.phone}`;
+  }
+
+  if (team.title === "Engineer") {
+    return `gitHub: ${employee.git}`;
+  }
+
+  if (team.title === "Intern") {
+      return `school: ${employee.school}`;
+  }
+
+}
+
+const generateTeam = memberArr => {
+  return `
+    <section class="my-3" id="portfolio">
+      <div class="flex-row justify-space-between">
+      ${memberArr
+        .map(({ name, id, email, ...header }) => {
+          return `
+          <div class="member-card">
+            <div class="card-top">
+                <h2>${teamArray[i].name}</h2>
+                <h2>${teamArray[i].role}</h2>
+            </div>
+            <ul class="list-group list-group-flush text">
+            <li class="list-group-item">ID: ${teamArray[j].id}</li>
+            <li class="list-group-item">Email: ${teamArray[j].email}</li>
+            <li class="list-group-item"> ${displayInfo(teamArray[i])}</li>
+        </ul>
+        `
+        })
+        .join('')}
+      </div>
+    </section>
+  `;
+};
+
+module.exports = templateData => {
+console.log(templateData);
+
+const { employee } = templateData;
+
+  return `
+  <!DOCTYPE html>
+  <html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Portfolio Demo</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css">
+    <link href="https://fonts.googleapis.com/css?family=Public+Sans:300i,300,500&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="style.css">
+  </head>
+  <body>
+    <header>
+      <div class="container flex-row justify-space-between align-center py-3">
+        <h1 class="page-title text-secondary bg-dark py-2 px-3">Team Profile</h1>
+      </div>
+    </header>
+    <main class="container my-5">
+      ${generateTeam(employee)}
+    </main>
+  </body>
+  </html>
+  `
+};
